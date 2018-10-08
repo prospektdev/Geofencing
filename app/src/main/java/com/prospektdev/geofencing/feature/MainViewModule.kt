8ac7d17c8ec53ff5291398593b7fde51fce10b50
Prospekt <prospektdevgroup.devops@gmail.com>
2018-10-08 17:57:33 +0300

    get user permission, add circle and on user position - marker

--- app/src/main/java/com/prospektdev/geofencing/feature/MainViewModule.kt
+++ app/src/main/java/com/prospektdev/geofencing/feature/MainViewModule.kt
@@ -1,8 +1,24 @@
 package com.prospektdev.geofencing.feature
 
+import android.arch.lifecycle.MutableLiveData
+import android.location.Location
 import com.prospektdev.geofencing.core.base.BaseViewModel
+import com.prospektdev.geofencing.model.location.LocationSource
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class MainViewModule @Inject constructor() : BaseViewModel() {
+    @Inject
+    lateinit var locationManager: LocationSource
+    var userLocation: MutableLiveData<Location> = MutableLiveData()
 
+    fun getLastKnownLocation(){
+       locationManager.getLastKnownLocation()
+               .subscribeOn(Schedulers.io())
+               .observeOn(AndroidSchedulers.mainThread())
+               .subscribe({location-> userLocation.value = location}) {}
+    }
 }
--- app/src/main/java/com/prospektdev/geofencing/feature/MapsActivity.kt
+++ app/src/main/java/com/prospektdev/geofencing/feature/MapsActivity.kt
@@ -1,36 +1,66 @@
 package com.prospektdev.geofencing.feature
 
+import android.arch.lifecycle.Observer
 import android.os.Bundle
+import android.util.Log
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import com.prospektdev.geofencing.R
 import com.prospektdev.geofencing.core.base.BaseActivity
+import com.prospektdev.geofencing.model.location.CheckPermission
+import com.prospektdev.geofencing.model.location.LocationManager
 import javax.inject.Inject
 
-class MapsActivity : BaseActivity(), OnMapReadyCallback {
 
+class MapsActivity : BaseActivity(), OnMapReadyCallback {
     @Inject
     lateinit var viewModel: MainViewModule
 
     private lateinit var mMap: GoogleMap
+    private lateinit var locationManager: LocationManager
+    private lateinit var currentPosition: LatLng
+    private lateinit var circle :Circle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        locationManager = LocationManager(this)
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        if (!CheckPermission.checkPermission(this)) {
+            CheckPermission.requestPermission(this, 256)
+        }
     }
 
     override fun obtainLayoutResId(): Int = R.layout.activity_maps
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        viewModel.userLocation.observe(this, Observer {
+            if (it != null) {
+                currentPosition = LatLng(it.latitude, it.longitude)
+                Log.d("Location", "latitude - " + it.latitude + " long -" + it.longitude)
+                mMap.addMarker(MarkerOptions().position(currentPosition).title("It's Me"))
+                val cameraPosition = CameraPosition.Builder()
+                        .target(currentPosition)
+                        .zoom(17f)
+                        .bearing(90f)
+                        .tilt(30f)
+                        .build()
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+            }
+        })
+        viewModel.getLastKnownLocation()
+
+        mMap.setOnMapClickListener {
+             circle = mMap.addCircle(CircleOptions()
+                    .radius(60.0)
+                    .center(it)
+                    .strokeWidth(2f)
+                    .fillColor(254))
+
+        }
     }
 }
